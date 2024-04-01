d7d9af64fe38a082bad3c3dc21fa26ed73d409d6
alumno informatica <alumno@ciis.usil.edu.pe>
2024-04-01 12:38:08 -0500

    char

--- S2/lab02.cpp
+++ S2/lab02.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include<vector>
 
+
 using namespace std;
+
 
 class UnionFind {
     private:
         vector<int> parent;
         vector<int> size;
+
 
     public:
         UnionFind(int n){
@@ -18,6 +21,7 @@ class UnionFind {
             }
         }
 
+
         int find(int x){
             if(parent[x] != x){
                 parent[x] = find(parent[x]);
@@ -25,12 +29,14 @@ class UnionFind {
             return parent[x];
         }
 
+
         void UnionSet(int x, int y){
             int rootX = find(x);
             int rootY = find(y);
             if(rootX == rootY){
                 return;
             }
+
 
             if(size[rootX] < size[rootY]){
                 parent[rootX] = rootY;
@@ -44,32 +50,41 @@ class UnionFind {
         }
 };
 
+
 void buscarEquivalencia(UnionFind uf,  int n1, int n2){
     cout << "Elemento " << n1 << " y " << n2 << " son equivalentes?? " ;
     if (uf.find(n1) == uf.find(n2)) cout << "SI." << endl;
     else cout << "NO." << endl;
+
 
 }
 int main()
 {
     UnionFind uf(12);
 
+
     uf.UnionSet(1,0);
     uf.UnionSet(2,0);
     uf.UnionSet(3,1);
 
+
     uf.UnionSet(5,4);
     uf.UnionSet(6,4);
     uf.UnionSet(7,6);
+
 
     uf.UnionSet(9,8);
     uf.UnionSet(10,8);
     uf.UnionSet(11,10);
 
 
+
+
     buscarEquivalencia(uf, 1, 3);
     buscarEquivalencia(uf, 2, 9);
     buscarEquivalencia(uf, 4, 7);
 
+
     return 0;
 }
+
