ba0fc2e7c02a7dfb1eaaa9fcf91c3a784dd03dd1
Piero010405 <84740254+Piero010405@users.noreply.github.com>
2024-04-22 19:46:52 -0500

    lab5 fixed

--- S5/Lab05.cpp
+++ S5/Lab05.cpp
@@ -1,31 +1,37 @@
 #include <iostream>
 #include <vector>
-#include<cctype>
+#include <cctype>
 using namespace std;
 const int TAMANO_ARREGLO = 26;
 const int INICIO_ASCCI = 97;
 
-struct NodoArbolPrefijo{
+struct NodoArbolPrefijo
+{
     bool esFinal;
-    NodoArbolPrefijo* hijos[TAMANO_ARREGLO];
+    NodoArbolPrefijo *hijos[TAMANO_ARREGLO];
     char c;
 };
 
-NodoArbolPrefijo* NuevoNodo(char c){
-    NodoArbolPrefijo* nodo = new NodoArbolPrefijo();
+NodoArbolPrefijo *NuevoNodo(char c)
+{
+    NodoArbolPrefijo *nodo = new NodoArbolPrefijo();
     nodo->esFinal = false;
-    for (int i = 0; i < TAMANO_ARREGLO; i++) {
-        nodo->hijos[i] = nullptr; 
+    for (int i = 0; i < TAMANO_ARREGLO; i++)
+    {
+        nodo->hijos[i] = nullptr;
     }
     nodo->c = c;
     return nodo;
 }
 
-void Insertar(string palabra, NodoArbolPrefijo *raiz){
-    NodoArbolPrefijo* nodoActual = raiz;
-    for (char c: palabra){
+void Insertar(string palabra, NodoArbolPrefijo *raiz)
+{
+    NodoArbolPrefijo *nodoActual = raiz;
+    for (char c : palabra)
+    {
         int indice = static_cast<int>(tolower(c)) - INICIO_ASCCI;
-        if (nodoActual->hijos[indice] == nullptr || nodoActual->hijos[indice]->c == ' '){
+        if (nodoActual->hijos[indice] == nullptr || nodoActual->hijos[indice]->c == ' ')
+        {
             nodoActual->hijos[indice] = NuevoNodo(tolower(c));
         }
         nodoActual = nodoActual->hijos[indice];
@@ -33,11 +39,14 @@ void Insertar(string palabra, NodoArbolPrefijo *raiz){
     nodoActual->esFinal = true;
 }
 
-bool Buscar(string palabra, NodoArbolPrefijo* raiz){
-    NodoArbolPrefijo* nodoActual = raiz;
-    for (char c: palabra){
+bool Buscar(string palabra, NodoArbolPrefijo *raiz)
+{
+    NodoArbolPrefijo *nodoActual = raiz;
+    for (char c : palabra)
+    {
         int indice = static_cast<int>(tolower(c)) - INICIO_ASCCI;
-        if (nodoActual->hijos[indice] == nullptr){
+        if (nodoActual->hijos[indice] == nullptr)
+        {
             return false;
         }
         nodoActual = nodoActual->hijos[indice];
@@ -45,30 +54,45 @@ bool Buscar(string palabra, NodoArbolPrefijo* raiz){
     return nodoActual != nullptr && nodoActual->esFinal;
 }
 
-void BuscarPalabra(string palabra, NodoArbolPrefijo* raiz){
-    if (Buscar(palabra, raiz)){
+void BuscarPalabra(string palabra, NodoArbolPrefijo *raiz)
+{
+    if (Buscar(palabra, raiz))
+    {
         cout << "La palabra '" << palabra << "' esta en el arbol de prefijos.";
-    } else {
+    }
+    else
+    {
         cout << "La palabra '" << palabra << "' NO esta en el arbol de prefijos.";
     }
 }
 
-void printEnOrder(NodoArbolPrefijo *nodo)
+void printEnOrder(NodoArbolPrefijo *nodo, int nivel = 0)
 {
     if (nodo == nullptr)
         return;
 
-    for (int i = 0; i < TAMANO_ARREGLO; i++){
-        if (nodo->hijos[i] != nullptr){
-            NodoArbolPrefijo* nodoActual = nodo->hijos[i];
-            cout << nodoActual->c << " - ";
-            printEnOrder(nodoActual);
-            cout << endl;
+    for (int i = 0; i < TAMANO_ARREGLO; i++)
+    {
+        if (nodo->hijos[i] != nullptr)
+        {
+            printEnOrder(nodo->hijos[i], nivel + 1);
         }
     }
+
+    if (nivel > 0)
+    {
+        for (int i = 0; i < nivel - 1; i++)
+        {
+            cout << "   ";
+        }
+        cout << "|--";
+    }
+
+    cout << nodo->c << endl;
 }
 
-void ImprimirArbol(NodoArbolPrefijo* arbol){
+void ImprimirArbol(NodoArbolPrefijo *arbol)
+{
     if (arbol == nullptr)
     {
         cout << "Árbol vacío!" << endl;
@@ -77,10 +101,9 @@ void ImprimirArbol(NodoArbolPrefijo* arbol){
     printEnOrder(arbol);
 }
 
-
 int main()
 {
-    NodoArbolPrefijo* raiz = NuevoNodo(' ');
+    NodoArbolPrefijo *raiz = NuevoNodo(' ');
 
     // Insertar("hola", raiz);
     // Insertar("adios", raiz);
@@ -92,24 +115,24 @@ int main()
 
     // BuscarPalabra("hola", raiz);
     // BuscarPalabra("adios", raiz);
-    
+
     // EJERCICIO 1
-    Insertar("Romane", raiz);
-    Insertar("Romanus", raiz);
-    Insertar("Rubens", raiz);
-    Insertar("Ruber", raiz);
-    Insertar("Rubicon", raiz);
-    Insertar("Rubicundus", raiz);
-    ImprimirArbol(raiz);
+    // Insertar("Romane", raiz);
+    // Insertar("Romanus", raiz);
+    // Insertar("Rubens", raiz);
+    // Insertar("Ruber", raiz);
+    // Insertar("Rubicon", raiz);
+    // Insertar("Rubicundus", raiz);
 
     // EJERCICIO 2
-    // Insertar("Algo", raiz);
-    // Insertar("Ala", raiz);
-    // Insertar("Abeja", raiz);
-    // Insertar("Trio", raiz);
-    // Insertar("Trigo", raiz);
-    // Insertar("Mama", raiz);
-    // Insertar("Manila", raiz);
-    // Insertar("Manipular", raiz);
+    Insertar("Algo", raiz);
+    Insertar("Ala", raiz);
+    Insertar("Abeja", raiz);
+    Insertar("Trio", raiz);
+    Insertar("Trigo", raiz);
+    Insertar("Mama", raiz);
+    Insertar("Manila", raiz);
+    Insertar("Manipular", raiz);
+    ImprimirArbol(raiz);
     return 0;
 }
